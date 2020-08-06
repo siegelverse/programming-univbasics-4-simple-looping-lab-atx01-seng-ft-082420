fd8d7f5b7d323f052844f170a907d0595096a6d1
Marcus Siegel <msiegel37@gmail.com>
2020-08-06 23:07:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,4 +10,24 @@ end
 def loop_message_n_times(message, number)
   count = 0 
   while count < number do 
-    puts ""
+    puts message
+    count += 1 
+  end
+end
+
+def output_array(array)
+  count = 0 
+  while count < array.length do 
+    puts array[count]
+    count += 1 
+  end 
+end 
+
+def return_string_array(array)
+  count = 0 
+  while count < array.length do 
+    puts array[count].to_s 
+    count += 1 
+  end 
+  new_array
+end 
